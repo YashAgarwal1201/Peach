8e26a588aa85fd2c29325910831257183f0b5a57
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2022-11-17 10:53:02 +0530

    commit 2 17-11-2022

--- jsonPHP.php
+++ jsonPHP.php
@@ -20,10 +20,6 @@ function check_user_identity($u_name, $u_password) {
 				}
 			}
 		}
-		/*else {
-			$newUser = array()
-			array_push($jsonData['allUsersDetails'], $newUser);
-		}*/
 	}
 }
 
@@ -31,19 +27,6 @@ function add_new_user($u_name, $u_password, $u_dob, $u_mname, $u_email) { // add
 	GLOBAL $jsonData, $jsonFileName;
 	if (strtolower(gettype($jsonData)) == 'array') {
 		if(!in_array($u_name, array_keys($jsonData['allUsersDetails']))) {
-			/*$userObj = {
-				"masterPassword": $u_password,
-				"primaryUser": {
-					"userName" : $u_name,
-					"password" : $u_password,
-					"name" : $u_mname,
-					"isPrimary" : true,
-					"userDetails" : {
-						"age" : 22,
-						"email" : $u_email
-					}
-				}
-			};*/
 			$newUser = array( 
 				"masterPassword"=> $u_password,
 				"primaryUser"=> array (
@@ -67,7 +50,6 @@ function add_new_user($u_name, $u_password, $u_dob, $u_mname, $u_email) { // add
 					)
 				)
 			);
-			//$newUser = array($u_name => $userObj);
 			//array_push($jsonData['allUsersDetails'][$u_name], $newUser);
 			$jsonData['allUsersDetails'][$u_name] = $newUser;
 			$jsonData2 = json_encode($jsonData);
@@ -79,30 +61,4 @@ function add_new_user($u_name, $u_password, $u_dob, $u_mname, $u_email) { // add
 		}
 	}
 }
-
-//check_user_identity('@main_userYash','@main_userYash');
-/*
-//echo $jsonFileData;
-
-$jsonFileName = 'userData2.json';
-$jsonFileData = file_get_contents($jsonFileName); // get the json file content
-$jsonData = json_decode($jsonFileData, true); // decode the string format json file data in associative array format
-
-if (strtolower(gettype($jsonData)) == "array" ) {
-	passwordVerify($user_name, $input_password, $has_forgot);
-}
-$jsonData = json_decode($jsonFileData, true); // decode the string format json file data in associative array format
-
-// if datatype of $jsonData is array then only do anything ahead
-if (strtolower(gettype($jsonData)) == "array" ) {
-	print_r($jsonData);
-	$jsonData['allUsersDetails']["user2"] = array("name"=>"V", "isPrimary"=> False);
-	print_r($jsonData);
-	$jsonData2 = json_encode($jsonData);
-
-	$fp = fopen($jsonFileName, "w+");
-	fwrite($fp, $jsonData2);
-	fclose($fp);
-}*/
-
 ?>
